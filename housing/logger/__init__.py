b75e05f35002324b7e66e402a9fb3d94f71ce526
pradeepnegi88 <pradeepnegi2012@gmail.com>
2022-12-23 13:30:52 +0530

    logger added

---
+++ housing/logger/__init__.py
@@ -0,0 +1,24 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_DIR = 'logs'
+
+
+def get_current_time_stamp():
+    return f"{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"
+
+
+def get_log_file_name():
+    return f"log_{get_current_time_stamp()}.log"
+
+
+LOG_FILE_NAME = get_log_file_name()
+
+os.makedirs(LOG_DIR, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE_NAME)
+
+logging.basicConfig(filename=LOG_FILE_PATH, filemode="w",
+                    format='[%(asctime)s]^;%(levelname)s^;%(lineno)d^;%(filename)s^;%(funcName)s()^;%(message)s',
+                    level=logging.INFO)
